ca13e426a2bcf9a2816022dfbe1748c842960543
David <warrick.david58@gmail.com>
2026-01-09 13:14:53 -0800

    Add a "Disable All Non-Baseliner" choice to the xenotype chooser on the Faction Customization window

--- Source/FactionColonies/util/XenotypeFilter.cs
+++ Source/FactionColonies/util/XenotypeFilter.cs
@@ -26,7 +26,7 @@ namespace FactionColonies.util
             this.factionFc = factionFc;
             militaryUtil = factionFc.militaryCustomizationUtil;
             faction = DefDatabase<FactionDef>.GetNamed("PColony");
-            InitializeWithAllXenotypes();
+            InitializeXenotypes();
         }
 
         public void FinalizeInit(FactionFC factionFc)
@@ -37,25 +37,28 @@ namespace FactionColonies.util
 
             if (allowedXenotypes.Count == 0)
             {
-                InitializeWithAllXenotypes();
+                InitializeXenotypes();
             }
 
             RefreshPawnGroupMakers();
             WorldSettlementTraderTracker.reloadTraderKind();
         }
 
-        private void InitializeWithAllXenotypes()
+        private void InitializeXenotypes(bool initAllTypes = true)
         {
             allowedXenotypes.Clear();
             securityGuardsByXenotype.Clear();
 
             // Add all available xenotypes by default
-            foreach (XenotypeDef xenotype in DefDatabase<XenotypeDef>.AllDefsListForReading)
+            if (initAllTypes)
             {
-                if (xenotype.IsXenotypeWithLabel() && xenotype != XenotypeDefOf.Baseliner)
+                foreach (XenotypeDef xenotype in DefDatabase<XenotypeDef>.AllDefsListForReading)
                 {
-                    allowedXenotypes.Add(xenotype);
-                    SetupSecurityGuards(xenotype);
+                    if (xenotype.IsXenotypeWithLabel() && xenotype != XenotypeDefOf.Baseliner)
+                    {
+                        allowedXenotypes.Add(xenotype);
+                        SetupSecurityGuards(xenotype);
+                    }
                 }
             }
 
@@ -170,7 +173,12 @@ namespace FactionColonies.util
 
         public void ResetToAllXenotypes()
         {
-            InitializeWithAllXenotypes();
+            InitializeXenotypes();
+            RefreshPawnGroupMakers();
+        }
+        public void ResetToBaselinerXenotypeOnly()
+        {
+            InitializeXenotypes(false);
             RefreshPawnGroupMakers();
         }
 
