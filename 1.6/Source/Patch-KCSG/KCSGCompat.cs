d1f19081f6baa808e020e95c9cceef4e8ac18934
matathias <warrick.david58@gmail.com>
2026-03-16 21:03:51 -0700

    - Setup Empire's map gen to use its own generation steps - Add a (tentative) transpiler patch for Vanilla Expanded Framework's KCSG, to fix an out-of-bounds error

---
+++ 1.6/Source/Patch-KCSG/KCSGCompat.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using FactionColonies.util;
+using HarmonyLib;
+using KCSG;
+using Verse;
+
+namespace FactionColonies.KCSG
+{
+    /// <summary>
+    /// Compatibility patch for Vanilla Expanded Framework (KCSG module).
+    /// This assembly is only loaded when VEF is active (via LoadFolders.xml).
+    ///
+    /// Fixes IndexOutOfRangeException in KCSG's SettlementGenUtils.BuildingPlacement.CanPlaceAt
+    /// where roof clearance checks don't verify cells are within map bounds.
+    /// </summary>
+    [StaticConstructorOnStartup]
+    public static class KCSGCompatInit
+    {
+        static KCSGCompatInit()
+        {
+            new Harmony("com.Saakra.Empire.KCSG").PatchAll(Assembly.GetExecutingAssembly());
+            LogUtil.MessageForce("KCSG compatibility module loaded.");
+        }
+    }
+
+    [HarmonyPatch(typeof(SettlementGenUtils.BuildingPlacement))]
+    [HarmonyPatch("CanPlaceAt")]
+    public static class Patch_CanPlaceAt
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var roofedMethod = AccessTools.Method(typeof(GridsUtility), "Roofed",
+                new[] { typeof(IntVec3), typeof(Map) });
+            var safeMethod = AccessTools.Method(typeof(Patch_CanPlaceAt), "RoofedSafe");
+
+            var codes = instructions.ToList();
+            int replaced = 0;
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].Calls(roofedMethod))
+                {
+                    codes[i].operand = safeMethod;
+                    replaced++;
+                }
+            }
+
+            if (replaced == 0)
+                LogUtil.Warning("KCSG compat: Could not find Roofed call to patch in CanPlaceAt");
+
+            return codes;
+        }
+
+        public static bool RoofedSafe(IntVec3 c, Map map)
+        {
+            return c.InBounds(map) && c.Roofed(map);
+        }
+    }
+}
