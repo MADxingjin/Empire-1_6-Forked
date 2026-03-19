8cc9d9e115afcea5ee4cae51a02f739cf6a46f8c
matathias <warrick.david58@gmail.com>
2026-03-19 10:28:56 -0700

    Send the player a letter when bills are auto-resolved as a result of hitting their due date. Apply small penalties as well

--- 1.6/Source/Core/FactionColonies/settlements/BillFC.cs
+++ 1.6/Source/Core/FactionColonies/settlements/BillFC.cs
@@ -1,5 +1,6 @@
 ﻿using RimWorld;
 using System.Collections.Generic;
+using System.Linq;
 using Verse;
 
 namespace FactionColonies
@@ -144,12 +145,49 @@ namespace FactionColonies
         public static void ProcessBills()
         {
             FactionFC factionfc = FactionCache.FactionComp;
+            List<WorldSettlementFC> latePaidSettlements = new List<WorldSettlementFC>();
+
             for (int i = factionfc.Bills.Count - 1; i >= 0; i--)
             {
                 if (factionfc.Bills[i].dueTick < Find.TickManager.TicksGame)
                 {
-                    factionfc.Bills[i].Resolve();
+                    BillFC bill = factionfc.Bills[i];
+                    bool owedMoney = bill.taxes.silverAmount < 0;
+                    WorldSettlementFC settlement = bill.settlement;
+
+                    if (bill.AttemptResolve())
+                    {
+                        if (owedMoney && settlement != null)
+                        {
+                            latePaidSettlements.Add(settlement);
+                        }
+                    }
+                    else
+                    {
+                        string messageString = "NotEnoughSilverForBill".Translate() + " "
+                            + settlement.Name + ". "
+                            + "ConfiscatedTithes".Translate() + "."
+                            + " " + "UnpaidTitheEffect".Translate();
+                        settlement.GainUnrestWithReason(new Message(messageString, MessageTypeDefOf.NegativeEvent), 10d);
+                        settlement.GainHappiness(-10d);
+                        factionfc.Bills.Remove(bill);
+                    }
                 }
+            }
+
+            if (latePaidSettlements.Count > 0)
+            {
+                foreach (WorldSettlementFC settlement in latePaidSettlements)
+                {
+                    settlement.GainUnrest(4d);
+                    settlement.GainHappiness(-4d);
+                }
+
+                string settlementList = string.Join("\n", latePaidSettlements.Select(s => "  - " + s.Name));
+                Find.LetterStack.ReceiveLetter(
+                    "LateBillAutoPaidLabel".Translate(),
+                    "LateBillAutoPaidDesc".Translate(latePaidSettlements.Count, settlementList),
+                    LetterDefOf.NegativeEvent);
             }
         }
     }
