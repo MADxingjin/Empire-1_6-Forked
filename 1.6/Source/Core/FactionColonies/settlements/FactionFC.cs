cd8ca2b7b2907a6094d5ffd0a285e920eabb07ee
matathias <warrick.david58@gmail.com>
2026-04-09 23:44:23 -0700

    1.2.2 - Move harmony.PatchAll into a static class with the StaticConstructorOnStartup tag

--- 1.6/Source/Core/FactionColonies/settlements/FactionFC.cs
+++ 1.6/Source/Core/FactionColonies/settlements/FactionFC.cs
@@ -210,64 +210,11 @@ namespace FactionColonies
 
         #region Constructor & Lifecycle
 
-        private static bool harmonyPatched = false;
-
         public FactionFC(World world) : base(world)
         {
-            if (!harmonyPatched)
-            {
-                var harmony = new Harmony("com.Matathias.Empire");
-
-                if (SystemInfo.operatingSystemFamily == OperatingSystemFamily.Linux)
-                {
-                    FixLinuxHarmonyCrash(harmony);
-                }
-
-                harmony.PatchAll();
-                harmonyPatched = true;
-            }
-        }
-
-        // Fix a crash related to a harmony bug on Linux
-        // This gets all patches Empire makes, gets the ones that would crash on Linux, and fixes them
-        static void FixLinuxHarmonyCrash(Harmony harmony)
-        {
-            bool WouldCrash(MethodInfo method)
-            {
-                if (method is null || !method.IsVirtual || method.IsAbstract || method.IsFinal)
-                {
-                    return false;
-                }
-
-                byte[] bytes = method.GetMethodBody()?.GetILAsByteArray();
-                if (bytes is null || bytes.Length == 0 || (bytes.Length == 1 && bytes.First() == 0x2A))
-                {
-                    return true;
-                }
-                return false;
-            }
-
-            var methods = typeof(FactionFC).Assembly.GetTypes().Where(t0 => t0 != null && t0.IsClass && !typeof(Delegate).IsAssignableFrom(t0) && t0.GetCustomAttributes(typeof(HarmonyPatch)).Any()).SelectMany(t1 =>
-            {
-                Type declaringType = null;
-                string methodName = null;
-                foreach (HarmonyPatch attr in t1.GetCustomAttributes(typeof(HarmonyPatch), false))
-                {
-                    if (attr.info.declaringType != null) declaringType = attr.info.declaringType;
-                    if (attr.info.methodName != null) methodName = attr.info.methodName;
-                }
-
-                MethodInfo[] m = declaringType?.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-                if (m is null) return new List<MethodInfo>();
-
-                return m.Where(met => met.Name == methodName);
-            }).Where(WouldCrash);
-
-            foreach (MethodInfo i in methods)
-            {
-                // Patching methods without any Prefixes/Postfixes before actually patching them fixes it. Idk why
-                harmony.Patch(i);
-            }
+            // We used to do the harmony patching here, but I moved it to HarmonyPatcher.cs with a
+            // [StaticConstructoreOnStartup] tag. Honestly not sure why the harmony patch was run here.
+            // Leaveing this comment mostly for posterity.
         }
 
         /// <summary>
